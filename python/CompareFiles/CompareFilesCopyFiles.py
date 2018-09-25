2e45d669802178b23fe85b3402c19939d61beec6
peter <peterhsun13@gmail.com>
2018-09-26 02:27:32 +0800

    "Try and Catch"

--- python/CompareFiles/CompareFilesCopyFiles.py
+++ python/CompareFiles/CompareFilesCopyFiles.py
@@ -9,7 +9,7 @@ import shutil
 import os
 import sys
 import os.path
-#pyinstaller -F .\hello.py
+# pyinstaller -F .\hello.py
 # mtime = time.ctime(os.stat("D:/datetime.txt").st_mtime) #文件修改時間
 # ctime = time.ctime(os.stat("D:/datetime.txt").st_ctime) #文件原始時間
 
@@ -17,71 +17,99 @@ import os.path
 loop = True
 while loop:
     choose = ''
-    choose = input('1：BackupFiles 2：CurrentFiles 3:CompareFiles 4:CopyFiles 5:Exit \n')
+    choose = input(
+        '1：BackupFiles 2：CurrentFiles 3:CompareFiles 4:CopyFiles 5:Exit \n')
     if choose == '1':
         myPathBackup = ''
         myPathBackup = input('請輸入路徑：')  # mypath = "G:\\NEW"
-        BackupFiles = open('BackupFiles.txt', 'w+', encoding='utf8')
+        backupFiles = open('BackupFiles.txt', 'w+', encoding='utf8')
         for root, dirs, files in walk(myPathBackup):  # 遞迴列出所有檔案的絕對路徑
             for f in files:
                 fullPathBackup = join(root, f)
                 mtime = str(datetime.fromtimestamp(
                     os.stat(fullPathBackup).st_mtime))  # 文件的修改时间
                 # print(fullpath,mtime)
-                BackupFiles.write(str(fullPathBackup.encode(
+                backupFiles.write(str(fullPathBackup.encode(
                     'utf-8').decode('utf-8')) + '|' + mtime + '\n')
                 #fullpathstr = str(fullpath.encode())
                 #print(f'{str(fullpath.encode())}', file = files)
-        BackupFiles.close()
+
+        backupFiles = open('BackupFiles.txt', 'r', encoding='utf8')
+        backupString = backupFiles.read()
+        backupFiles.close()
+        if len(backupString) == 0:
+            print('BackupFiles.txt裡無資料')
     elif choose == '2':
         myPathNew = ''
-        myPathNew = input('請輸入路徑：')
-    #mypath = "G:\\NEW"
-        CurrentFiles = open('CurrentFiles.txt', 'w+', encoding='utf8')
+        myPathNew = input('請輸入路徑：')  # mypath = "G:\\NEW"
+        currentFiles = open('CurrentFiles.txt', 'w+', encoding='utf8')
         for root, dirs, files in walk(myPathNew):  # 遞迴列出所有檔案的絕對路徑
             for f in files:
                 fullPathCurrent = join(root, f)
                 mtime = str(datetime.fromtimestamp(
                     os.stat(fullPathCurrent).st_mtime))  # 文件的修改时间
                 # print(fullpath,mtime)
-                CurrentFiles.write(str(fullPathCurrent.encode(
+                currentFiles.write(str(fullPathCurrent.encode(
                     'utf-8').decode('utf-8')) + '|' + mtime + '\n')
                 #fullpathstr = str(fullpath.encode())
                 #print(f'{str(fullpath.encode())}', file = files)
-        CurrentFiles.close()
+
+        currentFiles = open('CurrentFiles.txt', 'r', encoding='utf8')
+        currentString = currentFiles.read()
+        currentFiles.close()
+        if len(currentString) == 0:
+            print('CurrentFiles.txt裡無資料')
     elif choose == '3':
-        BackupFiles = open('BackupFiles.txt', 'r', encoding='utf8')
-        CurrentFiles = open('CurrentFiles.txt', 'r', encoding='utf8')
-        dff = set(BackupFiles).symmetric_difference(CurrentFiles)
-        # dff.discard('\n')
-        CompareFiles = open('CompareFiles.txt', 'w', encoding='utf8')
-        for line in dff:
-            CompareFiles.write(line + '\n')
-        BackupFiles.close()
-        CurrentFiles.close()
-        CompareFiles.close()
+        try:
+            backupFiles = open('BackupFiles.txt', 'r', encoding='utf8')
+            currentFiles = open('CurrentFiles.txt', 'r', encoding='utf8')
+            dff = set(backupFiles).symmetric_difference(currentFiles)
+            # dff.discard('\n')
+            compareFiles = open('CompareFiles.txt', 'w', encoding='utf8')
+            for line in dff:
+                compareFiles.write(line + '\n')
+            backupFiles.close()
+            currentFiles.close()
+            compareFiles = open('CompareFiles.txt', 'r', encoding='utf8')
+            compareString = compareFiles.read()
+            compareFiles.close()
+            if len(compareString) == 0:
+                print('CompareFiles.txt裡無資料')
+        except FileNotFoundError:
+            print('無BackupFiles.txt、CurrentFiles.txt檔案')
     elif choose == '4':
-        makeDirectory = os.getcwd()
-        f = open("CompareFiles.txt", "r+", encoding='utf-8')  # 開啟檔案,須以r+讀寫模式
-        filesList = f.readlines()
-        currentPath = os.path.abspath('')
-        for line in filesList:
-            data = line.strip()
-            if len(data) != 0:
-                filesPath = data.split('|')[0]
-                dirPath = filesPath.strip(filesPath.split('\\')[-1])
-                makeDirPath = dirPath.strip(filesPath.split('\\')[0])
+        try:
+            f = open('CompareFiles.txt', 'r+',
+                     encoding='utf-8')  # 開啟檔案,須以r+讀寫模式
+            filesString = f.read()
+            if len(filesString) == 0:
+                print('CompareFiles.txt裡無資料')
+            f = open('CompareFiles.txt', 'r+',
+                     encoding='utf-8')
+            filesList = f.readlines()
+            makeDirectory = os.getcwd()
+            currentPath = os.path.abspath('')
+            for line in filesList:
+                data = line.strip()
+                if len(data) != 0:
+                    filesPath = data.split('|')[0]
+                    dirPath = filesPath.strip(filesPath.split('\\')[-1])
+                    makeDirPath = dirPath.strip(filesPath.split('\\')[0])
                 # 如果有相同路徑不建立
-                if not os.path.isdir(makeDirectory+makeDirPath[:-1]):
-                    os.makedirs(makeDirectory+makeDirPath[:-1], 493)
-                try:
-                    # if os.path.isfile(filesPath):
-                    shutil.copy(filesPath, makeDirectory+makeDirPath[:-1])
-                except FileNotFoundError:
-                    print(filesPath + '此為刪除的檔案')
-                except:
-                    print('不明錯誤')
-        f.close()
+                    if not os.path.isdir(makeDirectory+makeDirPath[:-1]):
+                        os.makedirs(makeDirectory+makeDirPath[:-1], 493)                       
+                    try:
+                        # if os.path.isfile(filesPath):
+                        shutil.copy(filesPath, makeDirectory+makeDirPath[:-1])
+                    except FileNotFoundError:
+                        print(filesPath + '此為刪除的檔案')
+                    except:
+                        print('不明錯誤')
+            f.close()
+        except FileNotFoundError:
+            print('無CopareFiles.txt')
+        except NameError:
+            print('無CopareFiles.txt') 
     elif choose == '5':
         print('Bye~')
         loop = False
--- python/CompareFiles/test.py
+++ python/CompareFiles/test.py
@@ -2,28 +2,67 @@
 # -*- coding: UTF-8 -*-
 
 import shutil, os
+import sys
+import os
+import time
+import os.path
+ 
+makeDirectory = os.getcwd()
+f = open("CompareFiles.txt", "r+", encoding = 'utf-8')          #開啟檔案,須以r+讀寫模式
+filesList = f.readlines()
+currentPath = os.path.abspath('')
+for line in filesList:
+  data=line.strip()
+  if len(data)!=0:
+    filesPath = data.split('|')[0]
+    dirPath = filesPath.strip(filesPath.split('\\')[-1])
+    makeDirPath = dirPath.strip(filesPath.split('\\')[0])
+    if not os.path.isdir(makeDirectory+makeDirPath[:-1]): #如果有相同路徑不建立
+      os.makedirs(makeDirectory+makeDirPath[:-1], 493)    
+    try:
+      #if os.path.isfile(filesPath):
+       shutil.copy(filesPath, makeDirectory+makeDirPath[:-1])
+    except FileNotFoundError:
+      print(filesPath + '此為刪除的檔案')
+    except:
+      print('不明錯誤')
+f.close()
+    
+
 #2017-04-02 14:11:53.570550
 #filesCompare = open('FilesBackup.txt', 'r', encoding = 'utf8')
 #filesCompare.read()
-filea = open("filesCompare.txt", "r+", encoding = 'utf-8')          #開啟檔案,須以r+讀寫模式
-filea2 = open("filesCompare2.txt", "w+", encoding = 'utf-8')
-fileaString = filea.readlines()                    #將檔案讀成字串
+
+#r_file = open('filesCompare.txt', "r", encoding = 'utf-8')
+#lines = r_file.readlines()
+#r_file.close()
+#for idx, line in enumerate(lines):
+#    if line.split():
+#        print(line)
+#r_file.close()
+#print(fileaString)
 #print(fileaString)
 #filea.next()
 #idFilter = '|'         #搜索檔案內特定的文字
 #idPosition = fileaString.find(idFilter)  #抓出檔案內特定的文字位置
 #print(idPosition)
+#print(fileaList)
+#print(fileaString[:-idPosition])
+#print(idPosition)
 #filea.seek(idPosition,0)               #將當前檔案讀寫位置設定到想要改寫的地方
+#filea.write("123")
 #filea.write('\n')                  #將字串寫入，整數需要先更改成字串
 #filea.write('!@#')
 #print(fileaString)
 #filea.close()                                       #關閉檔案
-filesPath = os.path.abspath('')
-i = 0 
-for line in fileaString:
-  line = line.split('\n')
-  shutil.copy(line[i], filesPath)
-  i = i + 1
+#filesPath = os.path.abspath('')
+#i = 0 
+#for line in fileaList:
+  
+  #line = line.split('\n')
+  #print(line)
+  #shutil.copy(line[i], filesPath)
+  #i = i + 1
 
 
 #os.chdir('C:\\')
