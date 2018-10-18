8d68f1d3ef0045174b6d2325b16de1ff86381e79
chzhonge <chzhonge@gmail.com>
2018-10-18 21:59:53 +0800

    記錄一下 各種偷渡

--- python/CompareFiles/CompareFilesCopyFiles.py
+++ python/CompareFiles/CompareFilesCopyFiles.py
@@ -12,7 +12,7 @@ import os.path
 
 class CompareTool():
     def __init__(self):
-        self.systemMessage = '1：BackupFiles \n 2：CurrentFiles \n 3:CompareFiles \n 4:CopyFiles 5:Exit \n'
+        self.systemMessage = '1：BackupFiles \n 2：CurrentFiles \n 3:CompareFiles \n 4:CopyFiles \n 5:Exit \n'
         self.backUpFileName = 'BackupFiles.txt'
         self.newFileName = 'CurrentFiles.txt'
         self.compareFileName ='CompareFiles.txt'
@@ -27,6 +27,7 @@ class CompareTool():
 
     def main(self):
         while self.systemLoop:
+            os.system('cls')
             featureSwtich = input(self.systemMessage)
 
             if featureSwtich == self.backUpFileNameKey:
@@ -48,7 +49,11 @@ class CompareTool():
                     print(self.newFileNameKey + '無資料')
             elif featureSwtich == self.compareFileNameKey:
                 self.compareFile()
-
+            elif featureSwtich == '5':
+                print('Bye~')
+                self.systemLoop = False
+            else:
+                print('無此功能')
 
     def scanPath(self, filePath):
         content = ''
@@ -90,72 +95,14 @@ class CompareTool():
         except FileNotFoundError:
             print('無' + self.backUpFileName + '、' + self.newFileName + '檔案')
 
-loop = True
-while loop:
-    choose = ''
-    choose = input(
-        '1：BackupFiles 2：CurrentFiles 3:CompareFiles 4:CopyFiles 5:Exit \n')
-    if choose == '1':
-        myPathBackup = ''
-        myPathBackup = input('請輸入路徑：')
-        backupFiles = open('BackupFiles.txt', 'w+', encoding='utf8')
-        for root, dirs, files in walk(myPathBackup):  # 遞迴列出所有檔案的絕對路徑
-            for f in files:
-                fullPathBackup = join(root, f)
-                mtime = str(datetime.fromtimestamp(
-                    os.stat(fullPathBackup).st_mtime))
-
-                backupFiles.write(str(fullPathBackup.encode(
-                    'utf-8').decode('utf-8')) + '|' + mtime + '\n')
-
-        backupFiles = open('BackupFiles.txt', 'r', encoding='utf8')
-        backupString = backupFiles.read()
-        backupFiles.close()
-        if len(backupString) == 0:
-            print('BackupFiles.txt裡無資料')
-    elif choose == '2':
-        myPathNew = ''
-        myPathNew = input('請輸入路徑：')  # mypath = "G:\\NEW"
-        currentFiles = open('CurrentFiles.txt', 'w+', encoding='utf8')
-        for root, dirs, files in walk(myPathNew):  # 遞迴列出所有檔案的絕對路徑
-            for f in files:
-                fullPathCurrent = join(root, f)
-                mtime = str(datetime.fromtimestamp(
-                    os.stat(fullPathCurrent).st_mtime))  # 文件的修改时间
-                currentFiles.write(str(fullPathCurrent.encode(
-                    'utf-8').decode('utf-8')) + '|' + mtime + '\n')
-
-        currentFiles = open('CurrentFiles.txt', 'r', encoding='utf8')
-        currentString = currentFiles.read()
-        currentFiles.close()
-        if len(currentString) == 0:
-            print('CurrentFiles.txt裡無資料')
-    elif choose == '3':
-        try:
-            backupFiles = open('BackupFiles.txt', 'r', encoding='utf8')
-            currentFiles = open('CurrentFiles.txt', 'r', encoding='utf8')
-            dff = set(backupFiles).symmetric_difference(currentFiles)
-            compareFiles = open('CompareFiles.txt', 'w', encoding='utf8')
-            for line in dff:
-                compareFiles.write(line + '\n')
-            backupFiles.close()
-            currentFiles.close()
-            compareFiles = open('CompareFiles.txt', 'r', encoding='utf8')
-            compareString = compareFiles.read()
-            compareFiles.close()
-            if len(compareString) == 0:
-                print('CompareFiles.txt裡無資料')
-        except FileNotFoundError:
-            print('無BackupFiles.txt、CurrentFiles.txt檔案')
-    elif choose == '4':
+    def copyFile(self):
         try:
             f = open('CompareFiles.txt', 'r+',
                      encoding='utf-8')  # 開啟檔案,須以r+讀寫模式
             filesString = f.read()
             if len(filesString) == 0:
                 print('CompareFiles.txt裡無資料')
-            f = open('CompareFiles.txt', 'r+',
-                     encoding='utf-8')
+            f = open('CompareFiles.txt', 'r+', encoding='utf-8')
             filesList = f.readlines()
             makeDirectory = os.getcwd()
             currentPath = os.path.abspath('')
@@ -179,8 +126,6 @@ while loop:
             print('無CopareFiles.txt')
         except NameError:
             print('無CopareFiles.txt')
-    elif choose == '5':
-        print('Bye~')
-        loop = False
-    else:
-        print('無此功能')
+
+compareTool = CompareTool()
+compareTool.main()
